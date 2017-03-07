ba7acc5f4455ab4a30ec8f8380bb43d80ad49c2b
Lizbeth CCapira <lizbeth9589@gmail.com>
2017-03-07 14:29:20 -0500

    Arreglo

---
+++ js/mapa-2.js
@@ -0,0 +1,65 @@
+    
+function initMap() {
+    var myLatLng = {lat: -16.457148 , lng: -71.530759};
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: myLatLng,
+    disableDefaultUI: true
+  });
+    var myLatLng = {lat: -16.457148 , lng: -71.530759};
+    var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Aqui estoy',
+  });
+    var myLatLng1 = {lat: -16.459128, lng: -71.525970};
+    var marker = new google.maps.Marker({
+    position: myLatLng1,
+    map: map,
+    title: 'Miguel',
+    icon: "img/car1.png"
+  });
+    var myLatLng2 = {lat: -16.455101 , lng: -71.529467};
+    var marker = new google.maps.Marker({
+    position: myLatLng2,
+    map: map,
+    title: 'Mirrela',
+    icon: "img/car2.png"
+  });
+   
+    var myLatLng4 = {lat: -16.453259 , lng: -71.530539};
+    var marker = new google.maps.Marker({
+    position: myLatLng4,
+    map: map,
+    title: 'Helen',
+    icon: "img/car1.png"
+  });
+    var myLatLng5 =  {lat: -16.455101 , lng: -71.529467};
+    var marker = new google.maps.Marker({
+    position: myLatLng5,
+    map: map,
+    title: 'Jorge',
+    icon: "img/car2.png"
+  });
+     var myLatLng6 = {lat: -16.455348 , lng: -71.532789};
+    var marker = new google.maps.Marker({
+    position: myLatLng6,
+    map: map,
+    title: 'Josue',
+    icon: "img/car3.png"
+  });
+}
+
+function solicitarEstimado(){
+    $.ajax ({
+        url:"http://clientes.geekadvice.pe/api/estimado",
+     data:{tipo:"1"}
+    }).success(function(_data){
+        console.log(_data);
+        update(_data);
+    });
+} 
+ function update(_info){
+     alert(_info.destino);
+     alert(_info.estimado.min);
+ }
